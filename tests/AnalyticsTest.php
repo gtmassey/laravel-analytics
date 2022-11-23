6e1459ec0d276cc1aae521e6c2d2e06384b15ebf
Vytautas Smilingis <vytautas@whatagraph.com>
2022-11-23 17:16:42 +0100

    Added multiple ways to authorize credentials (#16)
    * Added mulitple ways to authorize credentials
    
    * Remove old readme line

--- tests/AnalyticsTest.php
+++ tests/AnalyticsTest.php
@@ -3,23 +3,56 @@
 namespace GarrettMassey\Analytics\Tests;
 
 use Carbon\CarbonImmutable;
-use GarrettMassey\Analytics\Facades\Analytics;
+use GarrettMassey\Analytics\Analytics;
 use Google\Analytics\Data\V1beta\BetaAnalyticsDataClient;
-use Illuminate\Support\Facades\App;
 use Mockery;
 use Mockery\MockInterface;
 
 class AnalyticsTest extends TestCase {
 
-	/** @test */
-	public function getClient_returns_GAClient()
+	public function test_client_init_with_default_credentials_env(): void
+	{
+		putenv('GOOGLE_APPLICATION_CREDENTIALS=' . storage_path('/framework/testing/disks/testing-storage/test-credentials.json'));
+		config()->set('analytics.credentials.file');
+
+		$this->assertInstanceOf(BetaAnalyticsDataClient::class, Analytics::query()->getClient());
+		putenv('GOOGLE_APPLICATION_CREDENTIALS=');
+	}
+
+	public function test_client_init_with_credentials_file(): void
 	{
 		$this->assertInstanceOf(BetaAnalyticsDataClient::class, Analytics::query()->getClient());
 	}
 
+	public function test_client_init_with_credentials_json_string(): void
+	{
+		config()->set('analytics.credentials.file');
+		config()->set('analytics.credentials.json', json_encode($this->credentials()));
 
+		$this->assertInstanceOf(BetaAnalyticsDataClient::class, Analytics::query()->getClient());
+	}
 
-	public function test_get_top_events()
+	public function test_client_init_with_credentials_array(): void
+	{
+		config()->set('analytics.credentials.file');
+		config()->set('analytics.credentials.array', $this->credentials());
+
+		$this->assertInstanceOf(BetaAnalyticsDataClient::class, Analytics::query()->getClient());
+	}
+
+	public function test_client_init_with_separate_credential_values(): void
+	{
+		config()->set('analytics.credentials.file');
+		$credentials = $this->credentials();
+
+		foreach ($credentials as $key => $value) {
+			config()->set('analytics.credentials.array.' . $key, $value);
+		}
+
+		$this->assertInstanceOf(BetaAnalyticsDataClient::class, Analytics::query()->getClient());
+	}
+
+	public function test_get_top_events(): void
 	{
 		CarbonImmutable::setTestNow(CarbonImmutable::create(2022, 11, 21));
 
@@ -46,8 +79,6 @@ class AnalyticsTest extends TestCase {
 		});
 
 		//TODO: assert response
-		Analytics::getTopEvents();
+		Analytics::query()->getTopEvents();
 	}
-
-
 }
